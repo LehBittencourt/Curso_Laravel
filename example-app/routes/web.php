68f32d177eb1ee77ea0795ee26848ea9601dc1ee
Letícia Bittencourt Vieira <bittencourt.vieira@academico.ifg.edu.br>
2024-06-13 13:42:38 -0300

    commit curso de laravel

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -8,20 +8,23 @@ Route::get('/', function () {
 });
 */
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
-// nome, categoria, assunto, mensagem
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 
-Route::get(
-    '/contato/{nome}/{categoria_id}', 
-    function(
-        string $nome = 'Desconhecido', 
-        int $categoria_id = 1
-    ) {
-        echo "Estamos aqui: $nome - $categoria_id";
-    }
-)->where('nome', '[A-Za-z]+')->where('categoria_id', '[0-9]+');
+Route::get('/login', function(){return 'Login';})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function() {return 'Forncedores';})->name('app.fornecedores');
+    Route::get('/produtos', function() {return 'Produtos';})->name('app.produtos');
+});
+
+Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class, 'teste'])->name('teste');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para págigna incial.'; 
+});
 
